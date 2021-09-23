5f1c56afa73d5d618e60ae099c114df889487d41
= <rtyenriques@gmail.com>
2021-09-23 12:47:18 -0700

    commit tree ac764a71daaaf83a3ebaa6a1db2f44bf3d8d11a1

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,9 @@
+class AppointmentsController < ApplicationController
+
+    def show
+        @appointment = Appointment.find_by(params[:id])
+
+    end
+
+
+end
---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,8 @@
+class DoctorsController < ApplicationController
+
+    def show
+        @doctor = Doctor.find_by_id(params[:id])
+
+    end
+
+end
